162ec384caf43c02bcdae7438647f7ce180f00a1
shajiquan <shajiquan@gmail.com>
2015-08-14 17:28:19 +0800

    optimize run/run.sh script

--- run/run.sh
+++ run/run.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 path_current_dir=$(cd `dirname $0`; pwd)
-path_gitit_dir=$(cd $path_current_dir;cd ..;pwd)
+path_parrent_dir=$(cd $path_current_dir;cd ..;pwd)
 
-cd $path_gitit_dir
+cd $path_parrent_dir
+
+conf_file=my-gitit.conf
+pid_file=pid-my-gitit.conf.pid
+log_file=logs-gitit.log
 
 case "$1" in
     start)
-        nohup gitit -f my-gitit.conf > logs-gitit.log  2>&1 &
+        nohup gitit -f $conf_file > $log_file  & echo $! > $pid_file 2>&1 &
         ;;
     restart)
-        ps aux |grep 'my-gitit.conf' | grep -v "grep" | awk '{print $2}' | xargs kill ; nohup gitit -f my-gitit.conf > logs-gitit.log 2>&1 &
+        cat $pid_file | xargs kill ; nohup gitit -f $conf_file > $log_file  & echo $! > $pid_file 2>&1 &
         ;;
     stop)
-        ps aux |grep 'my-gitit.conf' | grep -v "grep" | awk '{print $2}'  | xargs kill
+        cat $pid_file | xargs kill; echo "stoped $conf_file"
         ;;
     *)
         echo "Usage: $0 {start | stop | restart}" >&2; exit 1
     ;;
 esac
 
-exit 0
+exit 1
